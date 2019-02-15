19d204251df5cf754f40ce3cc89a6bc52d59cbae
Pablo Nuñez <pablonura91@gmail.com>
2019-02-15 09:32:27 +0100

    Create abstract class

---
+++ src/main/java/Figura.java
@@ -0,0 +1,11 @@
+public abstract class Figura {
+    double area, perimetro;
+
+    public double getArea(){
+        return area;
+    }
+
+    public double getPerimetro(){
+        return perimetro;
+    }
+}
