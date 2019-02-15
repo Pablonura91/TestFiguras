1cc2914130c53ac936fe0e0487d4ce65446bed1a
Pablo Nuñez <pablonura91@gmail.com>
2019-02-15 14:35:55 +0100

    refactor Rectangulo

--- src/main/java/Rectangulo.java
+++ src/main/java/Rectangulo.java
@@ -1,7 +1,26 @@
 public class Rectangulo extends Figura{
-    public Rectangulo(double area, double perimetro) {
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
         super();
-        this.area = area;
-        this.perimetro = perimetro;
+        if (base > 0 && altura > 0) {
+            this.base = base;
+            this.altura = altura;
+        }
+        else try {
+            throw new FigureElementNegativeException("You cannot have a negative base or altura in a rectangle");
+        } catch (FigureElementNegativeException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public double getArea() {
+        return base * altura;
+    }
+
+    @Override
+    public double getPerimetro() {
+        return 2 * (base + altura);
     }
 }
