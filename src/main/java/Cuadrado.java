32215a64bb5402a30b77104c2fd1a73c1c5e4826
Pablo Nuñez <pablonura91@gmail.com>
2019-02-15 09:31:32 +0100

    Create objects

---
+++ src/main/java/Cuadrado.java
@@ -0,0 +1,9 @@
+public class Cuadrado extends Figura {
+
+    public Cuadrado(double area, double perimetro) {
+        super();
+        this.area = area;
+        this.perimetro = perimetro;
+    }
+
+}
